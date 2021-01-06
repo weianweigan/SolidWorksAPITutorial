4e6bb4a9a2ea12f37b84f2ac040b02f38d963e41
dududu <1831197727@qq.com>
2021-01-06 14:32:37 +0800

    初始化

---
+++ src/ChapterOne.ConnectToSW/Program.cs
@@ -0,0 +1,33 @@
+﻿using SolidWorks.Interop.sldworks;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xarial.XCad.SolidWorks;
+
+namespace ChapterOne.ConnectToSW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sldProcess = Process.GetProcessesByName("SLDWORKS");
+
+            if (!sldProcess.Any())
+            {
+                Console.WriteLine("No SolidWorks opened");
+            }
+            else
+            {
+                var app = SwApplicationFactory.FromProcess(sldProcess.First());
+
+                ISldWorks sw = app.Sw;
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
