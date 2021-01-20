e389ab9d4914e6f4675b6fff598f51cc73a2792c
dududu <1831197727@qq.com>
2021-01-20 14:36:05 +0800

    1.连接SolidWorks，新建打开文档。

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# SolidWorks 二次开发教程
+
+使用C# 和 xCAD 构建SolidWorks应用程序
+
+## 1.教程地址
+
+### [bilibili视频](https://www.bilibili.com/video/BV1ya4y1n7xQ)
+
+## 2.已完成章节
+
+* 简介，打开新建文档
---
+++ src/Chapter3.PartAutomation/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using SolidWorks.Interop.sldworks;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using Xarial.XCad.SolidWorks;
+
+namespace Chapter3.PartAutomation
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ISwApplication _swApp;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var swProcess = Process.GetProcessesByName("SLDWORKS");
+            if (!swProcess.Any())
+            {
+                return;
+            }
+
+            _swApp = SwApplicationFactory.FromProcess(swProcess.First());
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double x = double.Parse(_xPostion.Text);
+            double y = double.Parse(_yPostion.Text);
+
+            var doc = _swApp.Sw.IActiveDoc2;
+            var featMgr = doc.FeatureManager;
+            var skeMgr = doc.SketchManager;
+
+            var feat = (featMgr.GetFeatures(true) as object[])
+                .Cast<IFeature>()
+                .Where(p => p.Name == "上视基准面")
+                .FirstOrDefault();
+
+            if (feat != null)
+            {
+                feat.Select2(false, 0);
+                skeMgr.InsertSketch(true);
+                var ske = skeMgr.ActiveSketch;
+                ske.
+            }
+        }
+    }
+}
